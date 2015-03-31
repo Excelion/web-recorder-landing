bed00ebc7228b7f30655feb112a82987c8fc2df4
Gleb Sevruk <gleb.sevruk@tobii.com>
2015-04-01 01:07:28 +0300

    delay-load

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,16 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('app', [
     'ngRoute',
+    'ngStorage',
     'ui.router',
     'landingPageControllers',
     'app.version'
 ]);
 
-
-
+// from https://blog.mariusschulz.com/2014/10/22/asynchronously-bootstrapping-angularjs-applications-with-server-side-data
+angular.element(document).ready(function() {
+    angular.bootstrap(document, ["app"]);
+});
 
 app.config(function($stateProvider,  $provide, $urlRouterProvider, $sceProvider, $routeProvider, $locationProvider) {
 
@@ -59,10 +62,50 @@ app.config(function($stateProvider,  $provide, $urlRouterProvider, $sceProvider,
         }
     };
 
+    var pageIdState = {
+        name: 'pageId', //mandatory. This counter-intuitive requirement addressed in issue #368
+        //    parent: home,  //mandatory
+        url: '/pageId',
+        views: {
+            'contentView' : {
+                controller: 'PageIdController',
+                templateUrl: 'partials/pageId.html'
+            },
+            menuView : instanceOfMenu
+        }
+    };
+
+    var pageDetailsState = {
+        name: 'pageDetails', //mandatory. This counter-intuitive requirement addressed in issue #368
+        //    parent: home,  //mandatory
+        url: '/pageDetails/:id',
+        views: {
+            'contentView' : {
+                controller: 'PageDetailsController',
+                templateUrl: 'partials/pageDetails.html'
+            },
+            menuView : instanceOfMenu
+        }
+    };
+    var longLoagingState = {
+        name: 'loadTimeout', //mandatory. This counter-intuitive requirement addressed in issue #368
+        //    parent: home,  //mandatory
+        url: '/loadTimeout',
+        views: {
+            'contentView' : {
+                controller: 'PageLoadTimeoutController',
+                templateUrl: 'partials/loadTimeout.html'
+            },
+            menuView : instanceOfMenu
+        }
+    };
   $stateProvider
       .state(homeState)
       .state(firstState)
-      .state(secondState);
+      .state(secondState)
+      .state(pageIdState)
+      .state(pageDetailsState)
+      .state(longLoagingState);
 
     $urlRouterProvider.when('', '/home')
 
@@ -82,6 +125,35 @@ app.config(function($provide) {
         };
         return $delegate;
     });
+});
+
+app.directive('validNumber', function() {
+    return {
+        require: '?ngModel',
+        link: function(scope, element, attrs, ngModelCtrl) {
+            if(!ngModelCtrl) {
+                return;
+            }
+
+            ngModelCtrl.$parsers.push(function(val) {
+                if (angular.isUndefined(val)) {
+                    var val = '';
+                }
+                var clean = val.replace( /[^0-9]+/g, '');
+                if (val !== clean) {
+                    ngModelCtrl.$setViewValue(clean);
+                    ngModelCtrl.$render();
+                }
+                return clean;
+            });
+
+            element.bind('keypress', function(event) {
+                if(event.keyCode === 32) {
+                    event.preventDefault();
+                }
+            });
+        }
+    };
 });
 
 app.run(function($rootScope, $state, $window) {
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,9 +12,18 @@ landingPageControllers.controller('HomeController', function ($scope, $state) {
 
 });
 
+
+landingPageControllers.controller('PageLoadTimeoutController', function ($scope, $state, $timeout) {
+    $scope.loadCompleted = false;
+    $timeout(function(){   // simulate HTTP delay
+        $scope.loadCompleted = true;
+    }, 10000);
+    $scope.now = new Date();
+
+});
 landingPageControllers.controller('MenuController', function ($scope, $state, $window, $location) {
 
-    $scope.availableLinks = ["second" , "first", "home"];
+    $scope.availableLinks = ["pageId", "loadTimeout", "second" , "first", "home"];
 
     $scope.boom = function(stateName){
         // this is too light
@@ -42,3 +51,67 @@ landingPageControllers.controller('SecondController', ['$scope', function($scope
 
     $scope.x = 'hello';
 }]);
+
+var newPageDetails = function(evt, $scope, $localStorage, $window) {
+    var counter =  $scope.startCountFrom;
+    $localStorage.startCountFrom = JSON.stringify(counter);
+
+
+    $scope.startCountFrom =  counter + 1;
+    if (evt.which == 1) {
+        history.pushState(null, null, '#/pageDetails/' + counter);
+
+        $window.location.reload();
+        evt.preventDefault();
+    }
+};
+
+landingPageControllers.controller('PageIdController',  function($scope, $localStorage, $window) {
+
+
+    $scope.resetCounter = function() {
+        $localStorage.startCountFrom = 1;
+        $scope.startCountFrom =  1;
+    };
+
+    $scope.openPageId = function(evt) {
+        newPageDetails(evt, $scope, $localStorage, $window)
+    };
+
+    if (!$localStorage.startCountFrom)
+    {
+        $localStorage.startCountFrom = JSON.stringify(1);
+    }
+
+    $scope.startCountFrom = JSON.parse($localStorage.startCountFrom);
+});
+
+
+landingPageControllers.controller('PageDetailsController',  function($scope, $localStorage, $window, $stateParams) {
+    if (!$localStorage.startCountFrom)
+    {
+        $localStorage.startCountFrom = JSON.stringify(1);
+    }
+
+
+        if (parseInt($localStorage.startCountFrom) > parseInt($stateParams.id)) {
+            $scope.id = parseInt($localStorage.startCountFrom);
+        }
+        else {
+            $scope.id = parseInt($stateParams.id);
+        }
+
+    $scope.startCountFrom =  $scope.id + 1;
+    $localStorage.startCountFrom =  JSON.stringify($scope.startCountFrom);
+
+    $scope.resetCounter = function() {
+        $localStorage.startCountFrom = JSON.stringify(1);
+        $scope.startCountFrom =  1;
+    };
+
+    $scope.openPageId = function(evt) {
+        newPageDetails(evt, $scope, $localStorage, $window)
+    };
+
+
+});
