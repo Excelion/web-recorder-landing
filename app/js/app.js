3b637173901a93ff3fa47111f8e8eeabe97f9fb3
Gleb Sevruk <gleb.sevruk@tobii.com>
2015-03-31 09:51:16 +0300

    Redirects/hash/states now works even from F5

--- app/js/app.js
+++ app/js/app.js
@@ -8,48 +8,90 @@ var app = angular.module('app', [
     'app.version'
 ]);
 
-//app.config(function($routeProvider, $locationProvider) {
-//
-//
-//  //$routeProvider
-//  //    .when(
-//  //    '/', {
-//  //      redirectTo: '/home'
-//  //    })
-//  //    .when('/home', {
-//  //      templateUrl: 'partials/home.html'
-//  //    })
-//  //    .when('/home/first', {
-//  //      templateUrl: 'partials/first.html'
-//  //    })
-//  //    .when('/home/second', {
-//  //      templateUrl: 'partials/second.html'
-//  //    })
-//  //    .otherwise({
-//  //      redirectTo: '/home'
-//  //    });
-//  //    $locationProvider.html5Mode(true);
-//  //
-//  //  }
-//
-//    $locationProvider.html5Mode(true);
-//});
 
-app.config(function($stateProvider, $urlRouterProvider, $sceProvider) {
+
+
+app.config(function($stateProvider,  $provide, $urlRouterProvider, $sceProvider, $routeProvider, $locationProvider) {
 
   // For any unmatched url, redirect to /somepage
-  $urlRouterProvider.otherwise("/home");
+ // $urlRouterProvider.otherwise("/home");
+    var instanceOfMenu =  {
+        templateUrl: 'partials/menu.html',
+        controller: 'MenuController'
+    };
+
+    var homeState = {
+        name: 'home',  //mandatory
+        url: '/home',
+        views: {
+            'contentView' : {
+                controller: 'HomeController',
+                templateUrl: 'partials/home.html'
+            },
+            menuView : instanceOfMenu
+        }
+    };
+
+    var firstState  = {
+        name: 'first', //mandatory. This counter-intuitive requirement addressed in issue #368
+    //    parent: home,  //mandatory
+        url: '/first',
+        views: {
+            'contentView' : {
+                controller: 'FirstController',
+                templateUrl: 'partials/first.html'
+            },
+            menuView : instanceOfMenu
+        }
+    };
+
+
+    var secondState = {
+        name: 'second', //mandatory. This counter-intuitive requirement addressed in issue #368
+    //    parent: home,  //mandatory
+       url: '/second',
+        views: {
+            'contentView' : {
+                controller: 'SecondController',
+                templateUrl: 'partials/second.html'
+            },
+            menuView : instanceOfMenu
+        }
+    };
 
   $stateProvider
-      .state('home', {
-          url: '/home',
-          templateUrl: 'partials/home.html'
-      })
-      .state('first', {
-          url: '/first',
-          templateUrl: 'partials/first.html'
-      })
-      .state('second', {
-          url: '/second',
-          templateUrl: 'partials/second.html'
-      })});
+      .state(homeState)
+      .state(firstState)
+      .state(secondState);
+
+    $urlRouterProvider.when('', '/home')
+
+   // $locationProvider.html5Mode(true);
+
+   // $urlRouterProvider.when('', '/home');
+
+});
+app.config(function($provide) {
+    $provide.decorator('$state', function($delegate, $stateParams) {
+        $delegate.forceReload = function() {
+            return $delegate.go($delegate.current, $stateParams, {
+                reload: true,
+                inherit: false,
+                notify: true
+            });
+        };
+        return $delegate;
+    });
+});
+
+app.run(function($rootScope, $state, $window) {
+
+    $rootScope.time = new Date().toLocaleTimeString();
+
+    //$rootScope.$on("$locationChangeStart", function(event, next, current) {
+    //    if(next==current && next=='/first') {
+    //        event.preventDefault();
+    //        $state.go('home');
+    //    }
+    //});
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,42 @@
  */
 'use strict';
 
-var landingPageControllers = angular.module('landingPageControllers', []);
+var landingPageControllers = angular.module('landingPageControllers',  ['ui.router']);
 
 
-landingPageControllers.controller('EmptyController', ['$scope', function($scope) {
+landingPageControllers.controller('HomeController', function ($scope, $state) {
+
+    $scope.now = new Date();
+
+});
+
+landingPageControllers.controller('MenuController', function ($scope, $state, $window, $location) {
+
+    $scope.availableLinks = ["second" , "first", "home"];
+
+    $scope.boom = function(stateName){
+        // this is too light
+        var newTarget = $state.get(stateName);
+
+        // $window.location.assign(newTarget.url);
+        //$state.redirectTo(newTarget.url);
+
+
+        history.pushState(null,null,'#/' + stateName);
+
+        $window.location.reload();
+    };
+
+
+});
+
+landingPageControllers.controller('FirstController', ['$scope', function($scope) {
+
+    $scope.x = 'hello';
+}]);
+
+
+landingPageControllers.controller('SecondController', ['$scope', function($scope) {
 
     $scope.x = 'hello';
 }]);
